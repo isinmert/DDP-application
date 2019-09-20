8c89f4d7e5552952eddb5af62d8480bed6d3b6d3
isinmert <50641719+isinmert@users.noreply.github.com>
2019-09-19 20:37:52 -0500

    Update linear_dyn.m

--- Linear quadratic DDP/linear_dyn.m
+++ Linear quadratic DDP/linear_dyn.m
@@ -1,4 +1,8 @@
 function [ x, fx, fu ] = linear_dyn( x0,u, N )
+%   Dynamics of the linear system with double interator
+%   given the initial state and input sequence function generates
+%   state sequence
+
 x=zeros(2,N);
 x(:,1)=x0;
 dt=0.01;
